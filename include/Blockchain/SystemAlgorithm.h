b2364b4f900b4aa96bc0171633d3e63dbd9718da
Justinas Letukas <justinasletukas@gmail.com>
2025-10-30 02:34:14 +0200

    Transactions become available after 6 blocks, allows safe efficient multithreading
    still needs improvements on queueing transactions

--- include/Blockchain/SystemAlgorithm.h
+++ include/Blockchain/SystemAlgorithm.h
@@ -6,12 +6,14 @@
 #define SYSTEMALGORITHM_H
 #include "HashAlg/HashGenInterface.h"
 #include "HashAlg/MatrixHash.h"
+class Transaction;
 
 class SystemAlgorithm {
 protected:
     HashGenInterface *hash;
     static constexpr unsigned short int MAX_HASH_LENGTH{64};
     const std::string SYSTEM_VERSION{"1.0"};
+    Transaction *root = nullptr;
 
     SystemAlgorithm(): hash(new MatrixHash()) {
     }
